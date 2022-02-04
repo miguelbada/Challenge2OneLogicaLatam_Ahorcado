a9202fe6c87261ed6fc986ce1147f6219e119a74
miguelbada <miguelenriquebada07@gmail.com>
2022-02-04 11:36:06 -0300

    canvas dibujo ahorcado

--- javaSript/canvas.js
+++ javaSript/canvas.js
@@ -1,2 +1,50 @@
+var pantalla = document.querySelector("canvas");
+var pincel = pantalla.getContext("2d");
+
+pincel.fillStyle = "lightgrey";
+pincel.fillRect(0, 0, 1200 ,800);
 
 
+pincel.lineWidth = 5;
+
+function triangulo(contexto, inicioX, inicioY, cateto) {
+    contexto.beginPath();
+    contexto.moveTo(inicioX, inicioY);
+    contexto.lineTo(inicioX + cateto * 1.7, inicioY + cateto);
+    contexto.lineTo(inicioX - cateto * 1.7, inicioY + cateto);
+    contexto.closePath();
+    contexto.stroke();
+}
+
+function linea(contexto, inicioX, inicioY, finX, finY) {
+    contexto.beginPath();
+    contexto.moveTo(inicioX, inicioY);
+    contexto.lineTo(finX, finY);
+    contexto.closePath();
+    contexto.stroke();
+}
+
+function circulo(contexto, inicioX, inicioY, radio) {
+    contexto.beginPath();
+    contexto.arc(inicioX, inicioY ,radio, 0 ,2*Math.PI);
+    contexto.stroke();
+}
+
+function dibujarHorca() {
+    triangulo(pincel, 150, 700, 50);
+    linea(pincel, 150, 700, 150, 100);
+    linea(pincel, 150, 100, 400, 100);
+    linea(pincel, 400, 100, 400, 200);
+}
+
+function dibujarPersona() {
+    circulo(pincel, 400, 250, 50); //cabeza
+    linea(pincel, 400, 300, 400, 500); //cuerpo
+    linea(pincel, 400, 310, 340, 350); //brazo derecho
+    linea(pincel, 400, 310, 460, 350); //brazo izquierdo
+    linea(pincel, 400, 500, 330, 600); //pie derecho
+    linea(pincel, 400, 500, 470, 600); //pie izquierdo
+}
+
+dibujarHorca();
+dibujarPersona(); 
